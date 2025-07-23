44549cbc14ce8d7efc77d23447d6b6ffb8481133
abdu1o <a.kapara2006@gmail.com>
2025-07-23 22:55:50 +0300

    +api route example

---
+++ src/app/api/listings/route.js
@@ -0,0 +1,14 @@
+import clientPromise from "../../../../lib/mongodb";
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db('airbnb');
+    const listings = await db.collection('listings').find({}).limit(10).toArray();
+
+    return Response.json(listings);
+  } catch (err) {
+    console.error('MongoDB error:', err);
+    return new Response(JSON.stringify({ error: 'DB error' }), { status: 500 });
+  }
+}
