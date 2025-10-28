889800cd9143fba145426edee768a06c29b7bcd4
abdu1o <a.kapara2006@gmail.com>
2025-10-28 14:19:02 +0200

    super final fix

--- pages/api/who.js
+++
@@ -1,9 +0,0 @@
-import { saveWho } from "../../lib/filtersStorage";
-
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    saveWho(req.body);
-    return res.status(200).json({ message: "Who filters saved" });
-  }
-  res.status(405).json({ error: "Method not allowed" });
-}
